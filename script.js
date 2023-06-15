50c58ed6ce67c2ad1c8e23517a9e5ecad571d9ad
Nopendro Pakpahan <nopendro45@outlook.com>
2023-06-15 21:38:12 +0700

    memperbaiki fitur form

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzMdUSN03b051Oi613k42OfN8Cg0tE7RZB3R92Rp6JoxyOx93m80rKFU66Au27u7Vbr/exec'
 const form = document.forms['contact-form']
 const btnKirim = document.querySelector(".btn-kirim")
-const btnLoading = document.querySelector(".btn-loading")
 const myalert = document.querySelector(".my-alert")
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    
-    
+    btnKirim.classList.toggle('d-none');
+   
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
+        .then(response => {
+            btnKirim.classList.toggle('d-none');
+            myalert.classList.toggle('d-none');
+            form.reset();
+            console.log('Success!', response);
+        })
         .catch(error => console.error('Error!', error.message))
 });
-
